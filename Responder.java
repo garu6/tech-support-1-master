fefb71c9ac4f78b14f3beb28d7af36b4bd9b15e1
Edgar Alvarez <edgarfabero85@gmail.com>
2018-04-20 00:38:19 +0200

    añadido en la clase Inputreader la modificacion que separa la cadena en palabras para generar la respuesta

--- Responder.java
+++ Responder.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
-
+import java.util.HashMap;
 
 
 /**
@@ -15,6 +15,8 @@ public class Responder
     
     private Random aleatorio;
     private ArrayList<String> frases;
+    private HashMap<String, String> respuestas;
+    
     /**
      * Construct a Responder - nothing to do
      */
@@ -22,22 +24,42 @@ public class Responder
     {
         aleatorio = new Random();
         frases = new ArrayList<String>();
+        respuestas = new HashMap<String, String>();
+        respuestasMap();
     }
 
+    
+    private void respuestasMap(){
+        respuestas.put("lento", "yo creo que ese es un problema de tu hardware deverias actualizarlo");
+        respuestas.put("cuelga", "vuelve a instalar el sistema operativo desde cero");
+        respuestas.put("error", "voy a informar sobre ese error a mi superior espere un momento");
+        respuestas.put("enciende", "¿ha probado si el cable de corriente esta enchufado?");
+        respuestas.put("caro", "señor nuestros softwares siguen las tarifas extandares de la empresa");
+        
+        
+    }
+    
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String palabra)
     {
-        
-        frases.add("cuentenos su problema señor");
-        frases.add("Le pondremos en contacto con un especialista");
-        frases.add("tenga paciencia señor estamos saturados en este momento");
-        frases.add("Señor esta usted siendo borde conmigo, respeteme por favor");
-        frases.add("Le paso con mi superior que tenga un buen dia :) ");
-        int numero = aleatorio.nextInt(frases.size()+1);
-        String resultado = frases.get(numero);
-        return resultado;
+        String response = null;
+        response = respuestas.get(palabra);
+                if (response != null){
+                    return response;
+                    
+                }else {
+                
+                frases.add("cuentenos su problema señor");
+                frases.add("Le pondremos en contacto con un especialista");
+                frases.add("tenga paciencia señor estamos saturados en este momento");
+                frases.add("Señor esta usted siendo borde conmigo, respeteme por favor");
+                frases.add("Le paso con mi superior que tenga un buen dia :) ");
+                int numero = aleatorio.nextInt(frases.size()+1);
+                String resultado = frases.get(numero);
+                return resultado;
+            }
     }
 }
