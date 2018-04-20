f8be9e43e8bda5af8ee0ffc2472f2e5cec9841ab
Edgar Alvarez <edgarfabero85@gmail.com>
2018-04-20 02:23:32 +0200

    el programa ya responde a conjuntos de palabras

--- Responder.java
+++ Responder.java
@@ -15,7 +15,7 @@ public class Responder
     
     private Random aleatorio;
     private ArrayList<String> frases;
-    private HashMap<String, String> respuestas;
+    private HashMap<HashSet<String>, String> respuestas;
     
     /**
      * Construct a Responder - nothing to do
@@ -24,20 +24,38 @@ public class Responder
     {
         aleatorio = new Random();
         frases = new ArrayList<String>();
-        respuestas = new HashMap<String, String>();
-        respuestasMap();
+        respuestas = new HashMap<>();
+
+        frases.add("cuentenos su problema señor");
+        frases.add("Le pondremos en contacto con un especialista");
+        frases.add("tenga paciencia señor estamos saturados en este momento");
+        frases.add("Señor esta usted siendo borde conmigo, respeteme por favor");
+        frases.add("Le paso con mi superior que tenga un buen dia :) ");
+        
+        
+        HashSet<String> set01 = new HashSet<>();
+        set01.add("gratis");
+        set01.add("app");
+        
+        HashSet<String> set02 = new HashSet<>();
+        set02.add("gratis");
+
+        HashSet<String> set03 = new HashSet<>();
+        set03.add("app");
+        
+        HashSet<String> set04 = new HashSet<>();
+        set04.add("problema");
+        set04.add("linux");
+        set04.add("crasea");
+        
+        
+        respuestas.put(set04,"yo creo que ese es un problema de tu hardware deverias actualizarlo");
+        respuestas.put(set01,"voy a informar sobre ese error a mi superior espere un momento");
+        respuestas.put(set02,"vuelve a instalar el sistema operativo desde cero");
+        respuestas.put(set03,"¿ha probado si el cable de corriente esta enchufado?");
     }
 
-    
-    private void respuestasMap(){
-        respuestas.put("lento", "yo creo que ese es un problema de tu hardware deverias actualizarlo");
-        respuestas.put("cuelga", "vuelve a instalar el sistema operativo desde cero");
-        respuestas.put("error", "voy a informar sobre ese error a mi superior espere un momento");
-        respuestas.put("enciende", "¿ha probado si el cable de corriente esta enchufado?");
-        respuestas.put("caro", "señor nuestros softwares siguen las tarifas extandares de la empresa");
-        
-        
-    }
+   
     
     /**
      * Generate a response.
@@ -46,34 +64,18 @@ public class Responder
     public String generateResponse(HashSet<String> palabra)
     {
         String response = null;
-        String primera = "";
-        Iterator<String> primeraPalabra = palabra.iterator();
-
-        boolean  busqueda = true;
-        while (primeraPalabra.hasNext() && busqueda){
-            response = respuestas.get(primeraPalabra.next());
-            if (response != null){
-                    busqueda =false;
-                }
-        }
-        for (String elemento : palabra){
-            
-            response = respuestas.get(elemento);
-        }
-        response = respuestas.get(primera);
-                if (response != null){
-                    return response;
-                    
-                }else {
+       
+        response = respuestas.get(palabra);
+        
+        
+        
+        
+                if (response == null){
                 
-                frases.add("cuentenos su problema señor");
-                frases.add("Le pondremos en contacto con un especialista");
-                frases.add("tenga paciencia señor estamos saturados en este momento");
-                frases.add("Señor esta usted siendo borde conmigo, respeteme por favor");
-                frases.add("Le paso con mi superior que tenga un buen dia :) ");
-                int numero = aleatorio.nextInt(frases.size()+1);
-                String resultado = frases.get(numero);
-                return resultado;
+                response = frases.get(aleatorio.nextInt(frases.size()));
+                
+                    
             }
+        return response;
     }
 }
